0f9972444948f886aa06391d994068ca8ee159bd
Stanislav Angelovic <stanislav.angelovic@siemens.com>
2023-01-21 01:47:24 +0100

    refactor: simplify async D-Bus connection handling

--- include/sdbus-c++/Message.h
+++ include/sdbus-c++/Message.h
@@ -54,7 +54,6 @@ namespace sdbus {
     class MethodReply;
     namespace internal {
         class ISdBus;
-        class IConnection;
     }
 }
 
@@ -249,12 +248,11 @@ namespace sdbus {
         bool doesntExpectReply() const;
 
     protected:
-        MethodCall(void *msg, internal::ISdBus* sdbus, const internal::IConnection* connection, adopt_message_t) noexcept;
+        MethodCall(void *msg, internal::ISdBus* sdbus, adopt_message_t) noexcept;
 
     private:
         MethodReply sendWithReply(uint64_t timeout = 0) const;
         MethodReply sendWithNoReply() const;
-        const internal::IConnection* connection_{};
     };
 
     class MethodReply : public Message
