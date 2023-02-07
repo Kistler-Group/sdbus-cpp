0c841d55eb4a1d7df8bfc41a2b9ff838b3ac1bfd
Stanislav Angelovič <stanislav.angelovic@protonmail.com>
2023-02-07 11:47:37 +0100

    chore:test 2

--- src/MessageUtils.h
+++ src/MessageUtils.h
@@ -11,7 +11,7 @@
  * This file is part of sdbus-c++.
  *
  * sdbus-c++ is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
+ * under the terms of the GNU Lesser General Public License as published byyyy
  * the Free Software Foundation, either version 2.1 of the License, or
  * (at your option) any later version.
  *
