eae54ea608c4f182505b82b72efd99a8f2f4fb7d
Alex Cani <alexcani109@gmail.com>
2026-03-01 15:00:42 +0100

    feat(awaitable): add Awaitable class
    Prepare for C++20 coroutine support for asynchronous D-Bus method
    calls by introducing the Awaitable<T> class, enabling co_await syntax.
    This class contains the three necessary methods to support the co_await
    operator: await_ready, await_suspend and await_resume.
    
    A shared data structure is used to hold data needed by the callback
    that will resume the coroutine and feed in the result or exception. This
    work similarly to how a future/promise pair work.
    Potential race conditions in the multithreaded scenario are covered by
    atomic operations on an AwaitableState value, preventing situations
    that could lead to a double coroutine resume or permanent suspension.

---
+++ include/sdbus-c++/Awaitable.h
@@ -0,0 +1,135 @@
+/**
+ * (C) 2016 - 2021 KISTLER INSTRUMENTE AG, Winterthur, Switzerland
+ * (C) 2016 - 2026 Stanislav Angelovic <stanislav.angelovic@protonmail.com>
+ * (C) 2026 - Alex Cani <alexcani109@gmail.com>
+ *
+ * @file Awaitable.h
+ *
+ * Created on: Feb 28, 2026
+ * Project: sdbus-c++
+ * Description: High-level D-Bus IPC C++ library based on sd-bus
+ *
+ * This file is part of sdbus-c++.
+ *
+ * sdbus-c++ is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * sdbus-c++ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with sdbus-c++. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SDBUS_CXX_AWAITABLE_H_
+#define SDBUS_CXX_AWAITABLE_H_
+
+#include <atomic>
+#include <coroutine>
+#include <cstdint>
+#include <exception>
+#include <memory>
+#include <optional>
+#include <type_traits>
+#include <variant>
+
+namespace sdbus
+{
+
+/********************************************//**
+ * @enum AwaitableState
+ *
+ * Represents the lifecycle state of an asynchronous
+ * operation in the coroutine awaitable protocol.
+ * Used for atomic coordination between the coroutine
+ * and the D-Bus callback thread.
+ *
+ ***********************************************/
+enum class AwaitableState : uint8_t
+{
+    NotReady, // Initial state: callback hasn't fired yet
+    Waiting,  // Coroutine is suspended and waiting for callback
+    Completed // Callback completed, result is ready
+};
+
+// Shared data
+template <typename T>
+struct AwaitableData
+{
+    using result_type = std::conditional_t<std::is_void_v<T>, std::monostate, T>;
+    std::optional<result_type> result;
+    std::optional<std::exception_ptr> exception;
+    std::coroutine_handle<> handle;
+    std::atomic<AwaitableState> status{AwaitableState::NotReady};
+};
+
+/********************************************//**
+ * @class Awaitable
+ *
+ * A C++20 coroutine awaitable that represents an asynchronous
+ * operation. Allows suspending a coroutine until a D-Bus method
+ * call completes, then resuming with the result or exception.
+ *
+ * This is not a full-fledged coroutine type, but a simple awaitable
+ * that can be used with `co_await` to retrieve results of async D-Bus calls.
+ * This is independent of any specific coroutine framework or scheduler,
+ * as it relies on the D-Bus callback mechanism to resume the coroutine.
+ *
+ * You most likely don't need to use this class directly. Instead, use the
+ * respective low-level or high-level API functions that return an Awaitable
+ * instance, such as IProxy::callMethodAsync with with_awaitable_t tag,
+ * or the .getResultAsAwaitable() methods of the high-level API.
+ *
+ ***********************************************/
+template <typename T>
+class Awaitable
+{
+public:
+    explicit Awaitable(std::shared_ptr<AwaitableData<T>> data) : data_(std::move(data)) {};
+
+    // Called when the coroutine is co_await'ed. Returns true if the coroutine should be suspended.
+    [[nodiscard]] bool await_ready() const noexcept
+    {
+        return data_->status.load(std::memory_order_acquire) == AwaitableState::Completed;
+    }
+
+    // Called when the coroutine is suspended, returning false here will immediately
+    // resume the coroutine.
+    bool await_suspend(std::coroutine_handle<> handle) noexcept
+    {
+        data_->handle = handle;
+
+        // Attempt transition from NotReady to Waiting.
+        AwaitableState expected = AwaitableState::NotReady;
+        return data_->status.compare_exchange_strong(expected, AwaitableState::Waiting, std::memory_order_acq_rel);
+    }
+
+    // Called when the coroutine is resumed. Returns the result or throws the exception.
+    T await_resume() const
+    {
+        if (data_->exception)
+        {
+            std::rethrow_exception(*data_->exception);
+        }
+
+        if constexpr (std::is_void_v<T>)
+        {
+            return;
+        }
+        else
+        {
+            return std::move(*data_->result);
+        }
+    }
+
+private:
+    std::shared_ptr<AwaitableData<T>> data_;
+};
+
+} // namespace sdbus
+
+#endif // SDBUS_CXX_AWAITABLE_H_
